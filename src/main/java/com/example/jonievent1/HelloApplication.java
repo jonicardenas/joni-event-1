4bebc7f88c143ce865745ced79a8693837ed7ff4
Joan Nicolás Cárdenas Bastidas <joan.cardenas@correounivalle.edu.co>
2026-02-28 16:52:22 -0500

    Initial commit

--- src/main/java/com/example/jonievent1/HelloApplication.java
+++ src/main/java/com/example/jonievent1/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.jonievent1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +13,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        //Parent root = fxmlLoader.load();
+
+        VBox root = new VBox();
+        Label sapo = new Label("Sapo");
+        root.getChildren().add(sapo);
+        Button mamahuevo = new Button("Insultar");
+        root.getChildren().add(mamahuevo);
+
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
